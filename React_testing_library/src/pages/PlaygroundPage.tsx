d552026011ef3ad749fbbcb9ee962d1b4426a965
Cheshta17 <122281116+Cheshta17@users.noreply.github.com>
2024-11-29 16:51:49 +0530

    Add files via upload

---
+++ React_testing_library/src/pages/PlaygroundPage.tsx
@@ -0,0 +1,10 @@
+import ExpandableText from '../components/ExpandableText';
+import TermsAndConditions from '../components/TermsAndConditions';
+
+const PlaygroundPage = () => {
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis dolore magnam quia ducimus minima non, voluptate vero repudiandae error tenetur atque libero voluptas pariatur consequatur. Expedita aspernatur omnis sequi similique vero obcaecati, minima cupiditate maiores voluptatum mollitia ea perferendis at distinctio! Quia repellendus impedit deleniti fugiat delectus quos possimus, explicabo sint consequuntur reprehenderit corrupti maiores assumenda ipsam repellat inventore eaque cupiditate ipsa culpa. Neque delectus consequuntur, rem dolorem aperiam illo dolore iste praesentium ducimus hic magni cumque dolores repellendus non blanditiis eligendi ad. Nulla vero libero incidunt, accusamus unde laborum reprehenderit amet, cum neque adipisci, repellendus necessitatibus exercitationem modi omnis.
+  return <ExpandableText text="Short text" />;
+};
+
+export default PlaygroundPage;
+
